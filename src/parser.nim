6f36fa47b26c85f6ffbd14d8ea187ca612b54a20
Gianluca Scacco <gianluca.scacco@gmail.com>
2022-01-26 16:57:23 +0100

    then parser added

--- src/parser.nim
+++ src/parser.nim
@@ -4,7 +4,8 @@ type
   ParserError* = object of CatchableError
   ParserKind = enum
     pkString,
-    pkRegexp
+    pkRegexp,
+    pkSequence
 
   Parser = ref object
     case kind: ParserKind
@@ -13,6 +14,8 @@ type
     of pkRegexp:
       regexp: Regex
       definition: string
+    of pkSequence:
+      parsers: seq[Parser]
 
 proc parseString(target: string): Parser =
   Parser(kind: pkString, target: target)
@@ -20,20 +23,28 @@ proc parseString(target: string): Parser =
 proc parseRegexp(target: string): Parser =
   Parser(kind: pkRegexp, regexp: target.re, definition: target)
 
+proc then(p1: Parser, p2: Parser): Parser =
+  Parser(kind: pkSequence, parsers: @[p1, p2])
+
 proc map[T](target: string, op: proc (s: string): T): T =
   op(target)
 
-proc parse(p: Parser, input: string): string =
+proc parse(p: Parser, input: string): seq[string] =
   case p.kind
     of pkString:
       if input.startsWith(p.target):
-        return p.target
+        return @[p.target]
       raise newException(ParserError, fmt"Parser failure: <{p.target}> not found")
     of pkRegexp:
       let res = input.findAll(p.regexp)
       if input.startsWith(p.regexp):
-        return res[0]
+        return @[res[0]]
       raise newException(ParserError, fmt"Parser failure: <{p.definition}> not found")
+    of pkSequence:
+      let r1 = parse(p.parsers[0], input)[0]
+      let input2 = input.substr(r1.len, input.len-1)
+      let r2 = parse(p.parsers[1], input2)[0]
+      return @[r1, r2]
 
 
 when isMainModule:
@@ -42,4 +53,8 @@ when isMainModule:
   echo fooParser.parse("foobar")
   echo barParser.parse("bar !!")
   let intParser = parseRegexp("[0-9]+")
-  echo intParser.parse("123").map(parseInt)
+  echo intParser.parse("123")[0].map(parseInt)
+  let fooBarParser = fooParser.then(barParser)
+  echo fooBarParser.parse("foobar")
+  let fooIntParser = fooParser.then(intParser)
+  echo fooIntParser.parse("foo123")
